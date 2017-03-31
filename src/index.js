81ac97f710f858549fb8d5a9fd753c25042d0a7a
vrimar <vrimar@thirdeyetech.ca>
2017-03-30 21:16:47 -0400

    Add portal source

---
+++ src/index.js
@@ -0,0 +1,31 @@
+import m from 'mithril';
+
+const Portal = {
+	rootElement: null,
+	content: null,
+
+	oninit(vnode) {
+		let rootElement = document.createElement('div');
+		document.body.appendChild(rootElement);
+		this.rootElement = rootElement;
+		this.content = { view: () => vnode.children };
+		m.mount(this.rootElement, this.content);
+	},
+
+	onbeforeupdate(vnode) {
+		this.content.view = () => vnode.children;
+	},
+
+	onremove(vnode) {
+		if (document.body.contains(this.rootElement)) {
+			m.mount(this.rootElement, null);
+			document.body.removeChild(this.rootElement);
+		}
+	},
+
+	view(vnode) {
+		return null;
+	}
+}
+
+export default Portal;
