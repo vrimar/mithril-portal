0fd21c9a8a82aa3c0d81eeba1c788e253d95c9ff
vrimar <vrimar@thirdeyetech.ca>
2017-05-12 15:23:22 -0400

    Update tests

--- test/index.spec.js
+++ test/index.spec.js
@@ -1,35 +1,46 @@
-global.window = Object.assign(require('mithril/test-utils/domMock.js')(), require('mithril/test-utils/pushStateMock')())
-
 import m from 'mithril';
 import mq from 'mithril-query';
 import assert from 'assert';
 import Portal from '../src/';
 
 describe('mithril-portal', () => {
+  let portal = null;
+  let root = { view() { return portal } };
 
-  beforeEach(() => document.body.innerHTML = '')
+  afterEach(() => m.mount(document.body, null));
 
   it('Should append div to body', () => {
-    const output = mq(Portal);
-    assert.equal(document.body.lastChild, output.vnode.tag.rootElement);
-  })
+    portal = m(Portal);
+    m.mount(document.body, root);
+    assert.equal(document.body.lastChild, portal.state.rootElement);
+  });
 
   it('Should render children', () => {
-    const output = mq(Portal, m('h1', 'Children'));
+    portal = m(Portal, m('h1', 'Children'));
+    m.mount(document.body, root);
     assert(document.body.lastChild.querySelector('h1'));
-  })
+  });
 
-  // it('Should update children on redraw', () => {
-  // 	const output = mq(Portal);
-  // 	output.vnode.children = m('h2', 'Updated children');
-  // 	output.redraw();
-  // 	assert(document.body.lastChild.querySelector('h2'));
-  // })
+  it('Should update children on redraw', (done) => {
+    const container = {
+      updated: false,
+      view: ({ state }) => m(Portal, state.updated ? 'true' : 'false')
+    }
+
+    m.mount(document.body, container);
+    container.updated = true;
+    m.redraw();
+
+    setTimeout(() => {
+      const a = document.body.lastChild.textContent;
+      assert.equal(document.body.lastChild.textContent, 'true');
+      done();
+    }, 20);
+  });
 
   it('Should remove portal div from body on unmount', () => {
-    const output = mq(Portal);
-    output.onremove();
+    portal = m(Portal, m('h1', 'Children'));
+    m.mount(document.body, null);
     assert.equal(document.body.childNodes.length, 0);
-  })
-
-})
+  });
+});
